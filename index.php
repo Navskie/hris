e30512372ca13366b0fe4173de98b11f8a3791be
navs <ronnel.navarro2020@gmail.com>
2024-09-03 15:17:50 +0800

    remove database

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php include_once 'core/database.php' ?>
+  <?php include_once 'cores/database.php' ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HRIS</title>
