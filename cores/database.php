bd78b4738196f16a147397a6762780d503196c6b
navs <ronnel.navarro2020@gmail.com>
2024-09-03 15:18:43 +0800

    remove database

---
+++ cores/database.php
@@ -0,0 +1,14 @@
+<?php 
+
+  $host = 'localhost';
+  $dbName = 'hris';
+  $username = 'root';
+  $password = '';
+
+  $db = mysqli_connect($host, $username, $password, $dbName);
+
+  session_start();
+
+  // remove
+
+?>
